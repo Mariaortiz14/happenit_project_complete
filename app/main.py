9323ddf8edc4352b1c27b1b22728bd882da990d0
Mariaortiz14 <myito1612@gmail.com>
2025-04-20 17:37:31 -0500

    Preparado para Railway3.0

--- app/main.py
+++ app/main.py
@@ -1,22 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import users, events,pqr  
-from app.database import engine, Base
-from app.routers import comment
-
+from routers import users, events, pqr, comment
+from database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(users.router)
 app.include_router(events.router)
